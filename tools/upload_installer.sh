69fc106d838855b095e97ebdfd1d39c52139e189
Christos Hadjiaslanis <christos@openquery.io>
2021-02-08 09:36:27 +0000

    refactor: Installer uploader now points to getsynth-public bucket
    GitOrigin-RevId: cb0b3ab9245e9f6dc579197faf00602b79d6ab6c

--- tools/upload_installer.sh
+++ tools/upload_installer.sh
@@ -3,4 +3,4 @@
 gsutil -h "Content-Type:application/x-shellscript" \
        -h "Cache-Control:no-store, max-age=0" \
         cp install.sh \
-        gs://artifacts.getsynth.appspot.com/install/installer.sh
+        gs://getsynth-public/install/install.sh
