108bc8c46097dd4aa9d2a4738a036acd8f2a7e70
Christos Hadjiaslanis <christos@openquery.io>
2021-05-11 14:52:05 +0100

    fix: install script uploader
    GitOrigin-RevId: 043c2594dd257bc5941f6ffd7a6b0b5a862cd9a9

--- tools/upload_installer.sh
+++ tools/upload_installer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-bucket=${bucket:-openquery-synth-prod-public}
+
 gsutil -h "Content-Type:application/x-shellscript" \
        -h "Cache-Control:no-store, max-age=0" \
         cp install.sh \
-        gs://$bucket/install
+        gs://getsynth-public/install/install.sh
