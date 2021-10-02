4acde10f7e799c188fc7b98985084147517c2f5a
WiredSound <maxoblack@yahoo.com>
2021-10-02 18:34:36 +0100

    refactor: remove unnecessary submodule in content::date_time

--- core/src/schema/content/date_time.rs
+++ core/src/schema/content/date_time.rs
@@ -1,6 +1,8 @@
-use std::sync::Arc;
-
 use super::prelude::*;
+
+use chrono::format::{parse as strptime, StrftimeItems};
+
+use std::sync::Arc;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Clone, Hash, Serialize)]
 pub struct ChronoValueAndFormat {
@@ -131,177 +133,166 @@ pub struct DateTimeContent {
     pub end: Option<ChronoValue>,
 }
 
-pub use datetime_content::ChronoValueFormatter;
+#[derive(Debug)]
+pub struct ChronoValueFormatter<'a>(&'a str, Option<ChronoValueType>);
 
-pub mod datetime_content {
-    use super::{ChronoValue, ChronoValueType, DateTimeContent, Error};
-    use anyhow::Result;
-    use serde::{Deserialize, Serialize};
-
-    use chrono::{
-        format::{parse as strptime, StrftimeItems},
-        DateTime, FixedOffset,
-    };
-
-    #[derive(Debug)]
-    pub struct ChronoValueFormatter<'a>(&'a str, Option<ChronoValueType>);
-
-    impl<'a> ChronoValueFormatter<'a> {
-        pub fn new_with(src: &'a str, hint: Option<ChronoValueType>) -> Self {
-            Self(src, hint)
-        }
-
-        pub fn new(src: &'a str) -> Self {
-            Self::new_with(src, None)
-        }
-
-        pub fn parse(&self, content: &str) -> Result<ChronoValue> {
-            debug!(
-                "parsing a chrono content from string '{}' ({:?})",
-                content, self
-            );
-
-            let mut parsed = chrono::format::Parsed::new();
-
-            strptime(&mut parsed, content, StrftimeItems::new(self.0)).map_err(|err| {
-                failed!(
-                    target: Debug,
-                    "could not parse '{}' as a chrono content with fmt='{}': {}",
-                    content,
-                    self.0,
-                    err
-                )
-            })?;
-
-            if let Some(hint) = self.1 {
-                match hint {
-                    ChronoValueType::DateTime => Ok(ChronoValue::DateTime(parsed.to_datetime()?)),
-                    ChronoValueType::NaiveDateTime => Ok(ChronoValue::NaiveDateTime(
-                        parsed.to_naive_date()?.and_time(parsed.to_naive_time()?),
-                    )),
-                    ChronoValueType::NaiveDate => {
-                        Ok(ChronoValue::NaiveDate(parsed.to_naive_date()?))
-                    }
-                    ChronoValueType::NaiveTime => {
-                        Ok(ChronoValue::NaiveTime(parsed.to_naive_time()?))
-                    }
-                }
-            } else {
-                parsed
-                    .to_datetime()
-                    .map(ChronoValue::DateTime)
-                    .or_else(|err| {
-                        debug!("a chrono content failed to parse as a datetime: {}", err);
-                        parsed
-                            .to_naive_date()
-                            .map(|date| match parsed.to_naive_time() {
-                                Ok(time) => ChronoValue::NaiveDateTime(date.and_time(time)),
-                                Err(_) => ChronoValue::NaiveDate(date),
-                            })
-                            .or_else(|err| {
-                                debug!(
-                                    "a chrono content failed to parse as a naive datetime: {}",
-                                    err
-                                );
-                                Ok(ChronoValue::NaiveTime(parsed.to_naive_time()?))
-                            })
-                    })
-            }
-        }
-
-        #[allow(dead_code)]
-        fn parse_or_default_of(
-            &self,
-            opt: Option<String>,
-            def: DateTime<FixedOffset>,
-            hint: ChronoValueType,
-        ) -> Result<ChronoValue> {
-            match opt.map(|inner| self.parse(&inner)).transpose()? {
-                Some(inner) => Ok(inner),
-                None => {
-                    let default = ChronoValue::default_of(def, hint);
-                    let fmt = ChronoValueFormatter::new_with(self.0, None);
-                    fmt.parse(&fmt.format(&default)?)
-                }
-            }
-        }
-
-        pub fn format(&self, c: &ChronoValue) -> Result<String, Error> {
-            use std::fmt::Write;
-            let mut buf = String::new();
-            let display = match c {
-                ChronoValue::DateTime(dt) => dt.format(self.0),
-                ChronoValue::NaiveDateTime(n_dt) => n_dt.format(self.0),
-                ChronoValue::NaiveTime(n_t) => n_t.format(self.0),
-                ChronoValue::NaiveDate(n_d) => n_d.format(self.0),
-            };
-            buf.write_fmt(format_args!("{}", display)).map_err(|err| {
-                failed_crate!(
-                    target: Release,
-                    "could not format date time of type '{}' with '{}': {}",
-                    c.type_(),
-                    &self.0,
-                    err
-                )
-            })?;
-            buf.shrink_to_fit();
-            Ok(buf)
-        }
+impl<'a> ChronoValueFormatter<'a> {
+    pub fn new_with(src: &'a str, hint: Option<ChronoValueType>) -> Self {
+        Self(src, hint)
     }
 
-    #[derive(Debug, Serialize, Deserialize)]
-    pub(super) struct SerdeDateTimeContent {
-        format: String,
-        #[serde(rename = "subtype")]
-        type_: Option<ChronoValueType>,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        begin: Option<String>,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        end: Option<String>,
+    pub fn new(src: &'a str) -> Self {
+        Self::new_with(src, None)
     }
 
-    impl SerdeDateTimeContent {
-        pub(super) fn into_datetime_content(self) -> Result<DateTimeContent> {
-            debug!("interpreting a shadow datetime content {:?}", self);
+    pub fn parse(&self, content: &str) -> Result<ChronoValue> {
+        debug!(
+            "parsing a chrono content from string '{}' ({:?})",
+            content, self
+        );
 
-            let src = &self.format;
-            let fmt = ChronoValueFormatter::new_with(src, self.type_);
+        let mut parsed = chrono::format::Parsed::new();
 
-            let type_ = self.type_.unwrap_or_default();
-            let begin = self
-                .begin
-                .map(|begin| fmt.parse(begin.as_str()))
-                .transpose()?;
-            let end = self.end.map(|end| fmt.parse(end.as_str())).transpose()?;
+        strptime(&mut parsed, content, StrftimeItems::new(self.0)).map_err(|err| {
+            failed!(
+                target: Debug,
+                "could not parse '{}' as a chrono content with fmt='{}': {}",
+                content,
+                self.0,
+                err
+            )
+        })?;
 
-            let common_variant = begin
-                .as_ref()
-                .and_then(|begin| begin.common_variant(end.as_ref()?));
-
-            match common_variant {
-                Some(variant) if variant != type_ => Err(failed!(target: Release, "content types of 'begin' and 'end' mismatch: begin is a {:?}, end is a {:?}; this is not allowed here. Try specifying the 'type' field.", begin, end)),
-                _ => Ok(DateTimeContent {
-                    format: self.format,
-                    type_,
-                    begin,
-                    end,
+        if let Some(hint) = self.1 {
+            match hint {
+                ChronoValueType::DateTime => Ok(ChronoValue::DateTime(parsed.to_datetime()?)),
+                ChronoValueType::NaiveDateTime => Ok(ChronoValue::NaiveDateTime(
+                    parsed.to_naive_date()?.and_time(parsed.to_naive_time()?),
+                )),
+                ChronoValueType::NaiveDate => {
+                    Ok(ChronoValue::NaiveDate(parsed.to_naive_date()?))
+                }
+                ChronoValueType::NaiveTime => {
+                    Ok(ChronoValue::NaiveTime(parsed.to_naive_time()?))
+                }
+            }
+        } else {
+            parsed
+                .to_datetime()
+                .map(ChronoValue::DateTime)
+                .or_else(|err| {
+                    debug!("a chrono content failed to parse as a datetime: {}", err);
+                    parsed
+                        .to_naive_date()
+                        .map(|date| match parsed.to_naive_time() {
+                            Ok(time) => ChronoValue::NaiveDateTime(date.and_time(time)),
+                            Err(_) => ChronoValue::NaiveDate(date),
+                        })
+                        .or_else(|err| {
+                            debug!(
+                                "a chrono content failed to parse as a naive datetime: {}",
+                                err
+                            );
+                            Ok(ChronoValue::NaiveTime(parsed.to_naive_time()?))
+                        })
                 })
+        }
+    }
+
+    #[allow(dead_code)]
+    fn parse_or_default_of(
+        &self,
+        opt: Option<String>,
+        def: DateTime<FixedOffset>,
+        hint: ChronoValueType,
+    ) -> Result<ChronoValue> {
+        match opt.map(|inner| self.parse(&inner)).transpose()? {
+            Some(inner) => Ok(inner),
+            None => {
+                let default = ChronoValue::default_of(def, hint);
+                let fmt = ChronoValueFormatter::new_with(self.0, None);
+                fmt.parse(&fmt.format(&default)?)
             }
         }
+    }
 
-        pub(super) fn from_datetime_content(c: &DateTimeContent) -> Result<Self> {
-            let fmt = ChronoValueFormatter::new_with(&c.format, None);
-            Ok(Self {
-                format: c.format.to_string(),
-                type_: Some(c.type_),
-                begin: c
-                    .begin
-                    .as_ref()
-                    .map(|begin| fmt.format(begin))
-                    .transpose()?,
-                end: c.end.as_ref().map(|end| fmt.format(end)).transpose()?,
+    pub fn format(&self, c: &ChronoValue) -> Result<String, Error> {
+        use std::fmt::Write;
+        let mut buf = String::new();
+        let display = match c {
+            ChronoValue::DateTime(dt) => dt.format(self.0),
+            ChronoValue::NaiveDateTime(n_dt) => n_dt.format(self.0),
+            ChronoValue::NaiveTime(n_t) => n_t.format(self.0),
+            ChronoValue::NaiveDate(n_d) => n_d.format(self.0),
+        };
+        buf.write_fmt(format_args!("{}", display)).map_err(|err| {
+            failed_crate!(
+                target: Release,
+                "could not format date time of type '{}' with '{}': {}",
+                c.type_(),
+                &self.0,
+                err
+            )
+        })?;
+        buf.shrink_to_fit();
+        Ok(buf)
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct SerdeDateTimeContent {
+    format: String,
+    #[serde(rename = "subtype")]
+    type_: Option<ChronoValueType>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    begin: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    end: Option<String>,
+}
+
+impl SerdeDateTimeContent {
+    fn into_datetime_content(self) -> Result<DateTimeContent> {
+        debug!("interpreting a shadow datetime content {:?}", self);
+
+        let src = &self.format;
+        let fmt = ChronoValueFormatter::new_with(src, self.type_);
+
+        let type_ = self.type_.unwrap_or_default();
+        let begin = self
+            .begin
+            .map(|begin| fmt.parse(begin.as_str()))
+            .transpose()?;
+        let end = self.end.map(|end| fmt.parse(end.as_str())).transpose()?;
+
+        let common_variant = begin
+            .as_ref()
+            .and_then(|begin| begin.common_variant(end.as_ref()?));
+
+        match common_variant {
+            Some(variant) if variant != type_ => Err(
+                failed!(target: Release, "content types of 'begin' and 'end' mismatch: begin is a {:?}, end is a {:?}; this is not allowed here. Try specifying the 'type' field.", begin, end)
+            ),
+            _ => Ok(DateTimeContent {
+                format: self.format,
+                type_,
+                begin,
+                end,
             })
         }
+    }
+
+    fn from_datetime_content(c: &DateTimeContent) -> Result<Self> {
+        let fmt = ChronoValueFormatter::new_with(&c.format, None);
+        Ok(Self {
+            format: c.format.to_string(),
+            type_: Some(c.type_),
+            begin: c
+                .begin
+                .as_ref()
+                .map(|begin| fmt.format(begin))
+                .transpose()?,
+            end: c.end.as_ref().map(|end| fmt.format(end)).transpose()?,
+        })
     }
 }
 
@@ -311,7 +302,7 @@ impl Serialize for DateTimeContent {
     where
         S: serde::Serializer,
     {
-        datetime_content::SerdeDateTimeContent::from_datetime_content(self)
+        SerdeDateTimeContent::from_datetime_content(self)
             .map_err(S::Error::custom)
             .and_then(|content| content.serialize(serializer))
     }
@@ -322,7 +313,7 @@ impl<'de> Deserialize<'de> for DateTimeContent {
     where
         D: serde::Deserializer<'de>,
     {
-        datetime_content::SerdeDateTimeContent::deserialize(deserializer)
+        SerdeDateTimeContent::deserialize(deserializer)
             .and_then(|inter| inter.into_datetime_content().map_err(D::Error::custom))
     }
 }
