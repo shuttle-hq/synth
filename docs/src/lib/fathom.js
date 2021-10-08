8118fe0a024bec38024189fb891e71b5ea7da82b
Damien Broka <damienbroka@mailbox.org>
2021-10-08 15:32:35 +0100

    fix: fathom on docs

---
+++ docs/src/lib/fathom.js
@@ -0,0 +1,45 @@
+module.exports = function (context) {
+    const { siteConfig } = context;
+    const { themeConfig } = siteConfig;
+    const { fathomAnalytics } = themeConfig || {};
+
+    if (!fathomAnalytics) {
+        throw new Error(
+            `You need to specify 'fathomAnalytics' object in 'themeConfig' with 'siteId' field in it to use docusaurus-plugin-fathom`
+        );
+    }
+
+    let { siteId, customDomain = 'https://cdn.usefathom.com' } = fathomAnalytics;
+
+    if (!siteId) {
+        throw new Error(
+            `You specified the 'fathomAnalytics' object in 'themeConfig' but the 'siteId' field was missing. Please ensure this is not a mistake.`
+        );
+    }
+
+    const isProd = process.env.NODE_ENV === 'production';
+
+    return {
+        name: 'docusaurus-plugin-fathom',
+
+        injectHtmlTags() {
+            if (!isProd) {
+                return {};
+            }
+
+            return {
+                headTags: [
+                    {
+                        tagName: 'script',
+                        attributes: {
+                            defer: true,
+                            src: `${customDomain}/script.js`,
+                            spa: 'auto',
+                            site: siteId,
+                        },
+                    },
+                ],
+            };
+        },
+    };
+};
--- www/lib/constants.ts
+++ www/lib/constants.ts
@@ -13,3 +13,7 @@ export const SITE_DOMAINS = ['www.getsynth.com', 'getsynth.com']
 export const POSTHOG_API_KEY = 'L-AQtrFVtZGL_PjK2FbFLBR3oXNtfv8OrCD8ObyeBQo'
 
 export const FATHOM_API_KEY = 'QRVYRJEG'
+
+export const UPVOTY_SSOTOKEN = null
+export const UPVOTY_BASEURL = 'synth.upvoty.com'
+export const UPVOTY_BOARDHASH = null //'2379bc1cbc07107ab1e951812389f0fe3d093b661a640d041b043ab41808586d'
