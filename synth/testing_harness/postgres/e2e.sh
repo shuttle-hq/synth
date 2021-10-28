43ec400baf8ae0449cff9e654eb846375f516ee1
Pieter <pieter@chesedo.me>
2021-10-29 01:40:11 +0200

    refactor: align workflow test harness with local test

--- synth/testing_harness/postgres/e2e.sh
+++ synth/testing_harness/postgres/e2e.sh
@@ -1,53 +1,146 @@
 #!/bin/bash
-# Run tests
 
-# database harness information
-PORT=5433
-PASSWORD="mysecretpassword"
-NAME=postgres-synth-harness
+set -uo pipefail
 
-# go to script directory
-cd "$(dirname "${BASH_SOURCE[0]}")"
-# delete leftover config
-rm -f .synth/config.toml
-
-# 0. init workspace
-synth init || exit 1
-
-# 1. generate test
-echo "Running generate test"
-synth generate hospital_master | jq >hospital_data_generated.json || exit 1
-
-# check by diff against golden master
-diff hospital_data_generated.json hospital_data_generated_master.json || exit 1
-rm hospital_data_generated.json
-
-# 2. database import test
-docker build -t $NAME . || exit 1
-echo "Running database with container name $NAME on port $PORT with password $PASSWORD"
-CONTAINER=$(docker run -p $PORT:5432 -e POSTGRES_PASSWORD=$PASSWORD -d $NAME)
-
-sleep 1
-
-RESULT=0
-
-echo "Importing hospital_import namespace from hospital"
-synth import --from postgres://postgres:${PASSWORD}@127.0.0.1:$PORT/postgres hospital_import || RESULT=1
-
-echo "stopping container"
-docker stop "${CONTAINER}" >/dev/null
-
-# check by diff against golden master
-diff <(jq --sort-keys . hospital_import/*) <(jq --sort-keys . hospital_master/*) || RESULT=1
-
-# removing generated namespace files
-rm -f hospital_import/doctors.json hospital_import/hospitals.json hospital_import/patients.json || RESULT=1
-rmdir hospital_import
-
-rm -f .synth/config.toml
-rmdir .synth
-
-# fail if any of the commands failed
-if [ $RESULT -ne 0 ]; then
-    exit 1
+# load env vars
+if [ -f .env ]
+then
+  set -a
+  . .env
+  set +a
 fi
+
+SYNTH="synth"
+
+ERROR='\033[0;31m'
+INFO='\033[0;36m'
+DEBUG='\033[0;37m'
+NC='\033[0m' # No Color
+
+function help() {
+  echo "$1 <command>
+
+commands:
+  load-schema|Fills DB with test schema
+  test-generate|Test if 'synth generate' is correct
+  test-import|Test importing from postgres data created by 'load-schema'
+  test-warning|Test integer warnings
+  test-local|Run all test on a local machine using the container from 'up' (no need to call 'up' first)
+  up|Starts a local Docker instance for testing
+  down|Stops container started with 'up'
+  cleanup|Cleanup local files after a test run
+" | column -t -L -s"|"
+}
+
+function load-schema() {
+  psql -f 0_hospital_schema.sql postgres://postgres:$PASSWORD@localhost:$PORT/postgres || return 1
+  psql -f 1_hospital_data.sql postgres://postgres:$PASSWORD@localhost:$PORT/postgres || return 1
+}
+
+function test-generate() {
+  echo -e "${INFO}Test generate${NC}"
+  $SYNTH generate hospital_master | jq > hospital_data_generated.json
+  diff hospital_data_generated.json hospital_data_generated_master.json || { echo -e "${ERROR}Generated file does not match master${NC}"; return 1; }
+}
+
+function test-import() {
+  echo -e "${INFO}Testing import${NC}"
+  $SYNTH import --from postgres://postgres:${PASSWORD}@localhost:${PORT}/postgres hospital_import || { echo -e "${ERROR}Import failed${NC}"; return 1; }
+  diff <(jq --sort-keys . hospital_import/*) <(jq --sort-keys . hospital_master/*) || { echo -e "${ERROR}Import namespaces do not match${NC}"; return 1; }
+}
+
+function test-warning() {
+  echo -e "${INFO}Testing warnings${NC}"
+  psql -f warnings/0_warnings.sql postgres://postgres:$PASSWORD@localhost:$PORT/postgres
+  WARNINGS=$($SYNTH generate --size 10 --to postgres://postgres:$PASSWORD@localhost:$PORT/postgres warnings 2>&1)
+  if [[ "$WARNINGS" == *"warnings.int32"* && "$WARNINGS" == *"warnings.int64"* ]]
+  then
+    echo -e "${DEBUG}Expected warnings were emitted${NC}"
+  else
+    echo -e "${ERROR}Did not get expected warnings:${NC}"
+    echo "expected"
+    echo "[yyyy-mm-ddThh:MM:ssZ WARN synth::datasource::relational_datasource] Trying to put an unsigned u32 into a int4 typed column warnings.int32"
+    echo "[yyyy-mm-ddThh:MM:ssZ WARN synth::datasource::relational_datasource] Trying to put an unsigned u64 into a int8 typed column warnings.int64"
+    echo "got"
+    echo $WARNINGS
+    return 1
+  fi
+}
+
+function test-local() {
+  cargo build --bin synth --release
+  SYNTH="../../../target/release/synth"
+
+  up || return 1
+
+  result=0
+  test-generate || result=$?
+  test-import || result=$?
+  test-warning || result=$?
+
+  down
+  cleanup
+
+  echo -e "${DEBUG}Done${NC}"
+
+  return $result
+}
+
+function up() {
+  echo -e "${DEBUG}Starting container${NC}"
+  docker build -t $NAME . || return 1
+  echo -e "${DEBUG}Running database with container name $NAME on port $PORT with password $PASSWORD${NC}"
+  docker run --rm --name $NAME -p $PORT:5432 -e POSTGRES_PASSWORD=$PASSWORD -d $NAME > /dev/null
+
+  wait_count=0
+  while [ $(docker logs $NAME 2>&1 | grep -c "ready to accept connections") -lt 2 ]
+  do
+    range=$(printf "%${wait_count}s")
+    echo -en "\\r${DEBUG}Waiting for DB to come up${range// /.}${NC}"
+    wait_count=$((wait_count + 1))
+    sleep 1
+  done
+  echo
+}
+
+function down() {
+  echo -e "${DEBUG}Stopping container${NC}"
+  docker stop $NAME > /dev/null
+}
+
+function cleanup() {
+  echo -e "${DEBUG}Cleaning up local files${NC}"
+  rm -f hospital_data_generated.json
+  rm -Rf hospital_import
+  rm -Rf .synth
+}
+
+case "${1-*}" in
+  load-schema)
+    load-schema || exit 1
+    ;;
+  test-generate)
+    test-generate || exit 1
+    ;;
+  test-import)
+    test-import || exit 1
+    ;;
+  test-warning)
+    test-warning || exit 1
+    ;;
+  test-local)
+    test-local || exit 1
+    ;;
+  up)
+    up
+    ;;
+  down)
+    down
+    ;;
+  cleanup)
+    cleanup
+    ;;
+  *)
+    help $0
+    exit 1
+esac
