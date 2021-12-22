f60246dde7b2a11dc7ee6c30795b3d9a03b637f8
chesedo <pieter@chesedo.me>
2021-12-22 16:37:34 +0200

    test: bool and string

--- synth/testing_harness/postgres/0_all_schema.sql
+++ synth/testing_harness/postgres/0_all_schema.sql
@@ -3,5 +3,12 @@ drop table if exists synth;
 create table synth
 (
   id int primary key,
-  nully text
+  nully text,
+  bool boolean NOT NULL,
+  string_char char NOT NULL,
+  string_varchar varchar(5) NOT NULL,
+  string_text text NOT NULL,
+  string_bpchar bpchar(6) NOT NULL,
+  string_name name NOT NULL,
+  string_uuid uuid NOT NULL
 )
