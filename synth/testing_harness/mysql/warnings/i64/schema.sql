e0b476ffcdbc5fbf7f0dc009ec02586d6dadbc57
chesedo <pieter@chesedo.me>
2022-01-13 11:00:04 +0200

    test: mysql warnings

---
+++ synth/testing_harness/mysql/warnings/i64/schema.sql
@@ -0,0 +1,10 @@
+drop table if exists i64;
+
+create table i64
+(
+    -- Unofficial types
+    `smallint` smallint NOT NULL,
+    `int` int NOT NULL,
+    `integer` integer NOT NULL,
+    `bigint` bigint NOT NULL
+);
