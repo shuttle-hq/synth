f40d396a809c0cde5b3e3e69bab5faf1f892b515
Pieter <pieter@chesedo.me>
2022-02-02 02:17:24 +0200

    chore: move integration tests to cargo - part 1

--- synth/src/main.rs
+++ synth/src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use std::io;
 use std::thread;
 use std::thread::JoinHandle;
 use structopt::StructOpt;
@@ -7,6 +8,7 @@ use synth::cli::Cli;
 
 #[async_std::main]
 async fn main() -> Result<()> {
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("warn")).init();
     let args = Args::from_args();
     let cli = Cli::new()?;
 
@@ -16,17 +18,18 @@ async fn main() -> Result<()> {
         Args::Version => None,
         _ => Some(thread::spawn(synth::version::notify_new_version_message)),
     };
+    let output = io::stdout();
 
     #[cfg(feature = "telemetry")]
     synth::cli::telemetry::with_telemetry(
         args,
-        |args| cli.run(args),
+        |args| cli.run(args, output),
         || cli.get_telemetry_context(),
     )
     .await?;
 
     #[cfg(not(feature = "telemetry"))]
-    cli.run(args).await?;
+    cli.run(args, output).await?;
 
     if let Some(notify_handle) = notify_handle {
         print_notify(notify_handle);
