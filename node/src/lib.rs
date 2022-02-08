1680cff0f66d53dafa73022ea8b84db2d326cf3e
WiredSound <maxoblack@yahoo.com>
2022-02-08 21:22:37 +0000

    feat: create `Content` instances from Node

--- node/src/lib.rs
+++ node/src/lib.rs
@@ -1,11 +1,35 @@
 use neon::prelude::*;
+use neon_serde::{from_value, to_value};
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string("hello node"))
+use synth_core::{Content, Graph};
+
+struct JsContent(Content);
+
+impl Finalize for JsContent {}
+
+fn new_content(mut cx: FunctionContext) -> JsResult<JsValue> {
+    let schema = cx.argument::<JsValue>(0)?;
+
+    match from_value(&mut cx, schema) {
+        Ok(content) => {
+            let boxed = cx.boxed(JsContent(content));
+            Ok(boxed.upcast())
+        }
+        Err(e) => JsError::type_error(&mut cx, e.to_string()).map(|handle| handle.upcast()),
+    }
+}
+
+struct JsGraph(Graph);
+
+impl Finalize for JsGraph {}
+
+fn new_graph(mut cx: FunctionContext) -> JsResult<JsValue> {
+    let wrapped_content = cx.argument::<JsBox<JsContent>>(0)?;
+    todo!()
 }
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("hello", hello)?;
+    cx.export_function("Content", new_content)?;
     Ok(())
 }
