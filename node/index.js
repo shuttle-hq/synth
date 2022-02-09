b426e2acaf3e6e6054e5eb2becbaede883f89ffd
WiredSound <maxoblack@yahoo.com>
2022-02-09 19:37:51 +0000

    feat: document JavaScript API

--- node/index.js
+++ node/index.js
@@ -3,20 +3,36 @@
 const neon = require("./index.node");
 
 class Content {
+    /**
+     * Construct a new Synth schema which can then be sampled.
+     * @param schema The Synth schema to construct this object from.
+     */
     constructor(schema) {
         this._content = neon.new_content(schema);
     }
 
+    /**
+     * Create a new sampler that will generated data based on this schema.
+     * @param seed The RNG seed to use when sampling.
+     */
     sample(seed = 0) {
         return new Sampler(this._content, seed);
     }
 }
 
 class Sampler {
+    /**
+     * Construct a Synth sampler to generate values based on some schema.
+     * @param content The compiled schema from which to sample.
+     * @param seed The RNG seed to use when sampling.
+     */
     constructor(content, seed = 0) {
         this._sampler = neon.new_sampler(content, seed);
     }
 
+    /*
+     * Sample a value from the schema.
+     */
     next() {
         return neon.sampler_next(this._sampler);
     }
