fd0f76985c43b271698869c8268ee85b918c423d
WiredSound <maxoblack@yahoo.com>
2022-02-11 23:50:45 +0000

    fix: use proper syntax for private class members

--- node/index.js
+++ node/index.js
@@ -3,12 +3,14 @@
 const neon = require("./index.node");
 
 class Content {
+    #content
+
     /**
      * Construct a new Synth schema which can then be sampled.
      * @param schema The Synth schema to construct this object from.
      */
     constructor(schema) {
-        this._content = neon.new_content(schema);
+        this.#content = neon.new_content(schema);
     }
 
     /**
@@ -16,27 +18,29 @@ class Content {
      * @param seed The RNG seed to use when sampling.
      */
     sample(seed = 0) {
-        return new Sampler(neon.new_sampler(this._content, seed));
+        return new Sampler(neon.new_sampler(this.#content, seed));
     }
 
     /**
      * Create a new sampler with a random RNG seed.
      */
     sampleRandomSeed() {
-        return new Sampler(neon.new_sampler_random_seed(this._content));
+        return new Sampler(neon.new_sampler_random_seed(this.#content));
     }
 }
 
 class Sampler {
+    #sampler
+
     constructor(internal) {
-        this._sampler = internal;
+        this.#sampler = internal;
     }
 
     /*
      * Sample a value from the schema.
      */
     next() {
-        return neon.sampler_next(this._sampler);
+        return neon.sampler_next(this.#sampler);
     }
 
     [Symbol.iterator]() {
