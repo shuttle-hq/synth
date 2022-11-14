16dd3d7d08a099989beb16306e44fb044156265e
iamwacko <iamawacko@protonmail.com>
2022-11-13 20:57:52 -0800

    chore: supress clippy errors

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,5 +1,5 @@
 #![feature(box_patterns, error_iter, try_blocks)]
-#![allow(type_alias_bounds)]
+#![allow(type_alias_bounds, result_large_err, unwrap_or_else_default)]
 
 #[macro_export]
 macro_rules! derive_generator {
